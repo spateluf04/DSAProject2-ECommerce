c1781c62d31b8e837a950497ea0895aaff9f15a2
spateluf04 <s.patel4@ufl.edu>
2025-11-04 01:46:45 -0500

    graph header

--- include/Graph.h
+++ include/Graph.h
@@ -4,7 +4,17 @@
 #include <string>
 #include <vector>
 
-#ifndef GRAPH_H
-#define GRAPH_H
-
-#endif //GRAPH_H
+class Graph {
+    //adjacency list (for each product id, set of neighboring product ids)
+    std::unordered_map<std::string, std::unordered_set<std::string>> adj;
+public:
+    //edge is connection between two products
+    void addEdge(const std::string& a, const std::string& b);
+    //returns recommended products, default number of recomendations is 3
+    std::vector<std::string> suggest(const std::string& product_id, int limit = 3) const;
+    int nodeCount() const;
+    int edgeCount() const;
+    bool hasId(const std::string& id) const;
+    //set with neighbors of product
+    const std::unordered_set<std::string>& neighbors(const std::string& id) const;
+};
