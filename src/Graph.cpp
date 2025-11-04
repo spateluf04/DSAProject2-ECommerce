7fad5a57f7223d402b53957d50c92402d198f217
spateluf04 <s.patel4@ufl.edu>
2025-11-04 15:05:44 -0500

    add edge func

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,6 +3,15 @@
 #include <unordered_set>
 #include <algorithm>
 
+//undirected edge used to represent if products were purchased together
+void Graph::addEdge(const std::string & a, const std::string & b) {
+    if (a == b) {
+        return;
+    }
+    adj[a].insert(b);
+    adj[b].insert(a);
+}
+
 //
 // Created by Kush Patel on 11/3/25.
 //
