aac00ff50b466f35fffe2a54ac6b26185b4594c6
spateluf04 <s.patel4@ufl.edu>
2025-11-04 17:58:02 -0500

    graph cpp

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -12,6 +12,33 @@ void Graph::addEdge(const std::string & a, const std::string & b) {
     adj[b].insert(a);
 }
 
-//
-// Created by Kush Patel on 11/3/25.
-//
+//recommendation func
+std::vector<std::string>Graph::recommend(const std::string& productId, int limit) const {
+    std::vector<std::pair<std::string,int>> freq; //product,count pairs
+    std::unordered_map<std::string,int> count;
+    if (!adj.count(productId)) return {}; //checks to see if original product in graph
+    //iterate thru neighbors of original product
+    for (auto& i : adj.at(productId))
+        for (auto& j : adj.at(i)) //neighbors neighbor
+            if (j != productId) count[j]++;
+
+    for (auto& p : count) freq.push_back(p); //map entries sent to vector
+    std::sort(freq.begin(), freq.end(), [](auto& a, auto& b){return a.second > b.second;});
+    std::vector<std::string> result;
+    for (int i=0;i<limit && i<static_cast<int>(freq.size());++i) {
+        result.push_back(freq[i].first);
+    }
+    return result;
+}
+
+//node count
+int Graph::nodeCount() const {
+    return adj.size();
+}
+
+//amount of edges
+int Graph::edgeCount() const {
+    std::size_t sum = 0;
+    for (const auto& kv : adj) sum += kv.second.size();
+    return sum / 2;
+}
