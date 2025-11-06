2e7654f5b83d68db53cc7f19d4b59c5208913652
Dmehta06 <dipak.mehta@ufl.edu>
2025-11-06 16:48:10 -0500

    Update DataLoader.cpp

--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -12,7 +12,7 @@
 
 namespace ecomm {
 
-//Deal with whitespaces
+//Deals with the whitespaces
     static std::string trim(std::string s){
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c){ return !std::isspace(c); }));
         s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char c){ return !std::isspace(c); }).base(), s.end());
